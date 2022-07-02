cce99e25ccb9d883f09639bcf1f72ba6ed63bef6
KJordan1225 <102583256+KJordan1225@users.noreply.github.com>
2022-07-02 01:14:05 -0400

    added route and container div to add user page

---
+++ resources/views/backend/user/add_user.blade.php
@@ -0,0 +1,16 @@
+@extends('admin.admin_master')
+
+@section('content')
+
+<div class="content-wrapper">
+	  <div class="container-full">
+		<!-- Content Header (Page header) -->
+		
+
+	
+	  
+	  </div>
+  </div>
+
+
+@endsection
