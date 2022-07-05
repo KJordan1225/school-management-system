9b4fb6d15cffe2589453febd89be5d86a9df86ce
KJordan1225 <102583256+KJordan1225@users.noreply.github.com>
2022-07-05 01:47:03 -0400

    added view and add student groups

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Backend\UserController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\Setup\StudentClassController;
 use App\Http\Controllers\Backend\Setup\StudentYearController;
+use App\Http\Controllers\Backend\Setup\StudentGroupController;
 
 /*
 |--------------------------------------------------------------------------
@@ -85,6 +86,17 @@ Route::prefix('setups')->group(function(){
     Route::get('student/year/delete/{id}', [StudentYearController::class, 'StudentYearDelete'])->name('student.year.delete');
     // Route::get('/password/view', [ProfileController::class, 'PasswordView'])->name('password.view');
     // Route::post('/password/update', [ProfileController::class, 'PasswordUpdate'])->name('password.update');
+
+     // Student Group Routes
+     Route::get('student/group/view', [StudentGroupController::class, 'ViewGroup'])->name('student.group.view');
+     Route::get('student/group/add', [StudentGroupController::class, 'StudentGroupAdd'])->name('student.group.add');
+     Route::post('student/group/store', [StudentGroupController::class, 'StudentGroupStore'])->name('store.student.group');
+     Route::get('student/group/edit/{id}', [StudentGroupController::class, 'StudentGroupEdit'])->name('student.group.edit');
+     Route::post('student/group/update/{id}', [StudentGroupController::class, 'StudentGroupUpdate'])->name('student.group.update');
+     // Route::post('/store', [ProfileController::class, 'ProfileStore'])->name('profile.store');
+     Route::get('student/group/delete/{id}', [StudentGroupController::class, 'StudentGroupDelete'])->name('student.group.delete');
+     // Route::get('/password/view', [ProfileController::class, 'PasswordView'])->name('password.view');
+     // Route::post('/password/update', [ProfileController::class, 'PasswordUpdate'])->name('password.update');
 
 });
 
